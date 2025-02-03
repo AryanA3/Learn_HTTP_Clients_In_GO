96ece2e5044bd8ffeef80ae6bd0c7a3125cade26
AryanA3 <aryanagarkar09@gmail.com>
2025-02-03 19:55:09 +0530

    CH 4 done

---
+++ Chapter_4_URLs/1./main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/url"
+)
+
+func newParsedURL(urlString string) ParsedURL {
+	parsedUrl, err := url.Parse(urlString)
+	if err != nil {
+		return ParsedURL{}
+	}
+
+	pass, set := parsedUrl.User.Password()
+	if !set {
+		pass = ""
+	}
+
+	return ParsedURL{
+		protocol: parsedUrl.Scheme,
+		username: parsedUrl.User.Username(),
+		password: pass,
+		hostname: parsedUrl.Hostname(),
+		port:     parsedUrl.Port(),
+		pathname: parsedUrl.Path,
+		search:   parsedUrl.RawQuery,
+		hash:     parsedUrl.Fragment,
+	}
+}
